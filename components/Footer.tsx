576bd8343ccb86483f147479fef04a945066799f
kotahorii <khorii.micron@gmail.com>
2021-11-10 21:45:43 +0900

    footer created

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>
-      
+    <div className="bg-red-400 py-5 flex flex-col justify-center items-center text-white">
+      &copy; 2017 WebCamp
     </div>
   )
 }
